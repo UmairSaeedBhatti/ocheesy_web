69d7114e7be5ef82420f922e3084c3014589666a
Hasnat ul haq <hasnatulhaq@outlook.com>
2022-06-17 15:07:44 +0500

    updating site

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Ocheesy! Ocheesy! Ocheesy! is in progress</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Work in progress! </h2>
     </div>
+    </>
+    
   );
 }
 
